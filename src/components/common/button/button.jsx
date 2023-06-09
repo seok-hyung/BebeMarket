f9684376d6f8cc35dff2c788632cb0c406204b78
sujung-lim <sujunglim94@gmail.com>
2023-06-09 14:35:08 +0900

    💄Style: 버튼 컴포넌트 구현

---
+++ src/components/common/button/button.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import * as S from './Button.style.js';
+
+export function NextButton() {
+  return (
+    <div>
+      <S.NextButton>다음</S.NextButton>
+    </div>
+  );
+}
+
+export function DisabledNextButton() {
+  return (
+    <div>
+      <S.DisabledNextButton>다음</S.DisabledNextButton>
+    </div>
+  );
+}
+
+export function MediumFollowButton() {
+  return (
+    <div>
+      <S.MediumFollowButton>팔로우</S.MediumFollowButton>
+    </div>
+  );
+}
+
+export function DisabledFollowButton() {
+  return (
+    <div>
+      <S.DisabledFollowButton>팔로우</S.DisabledFollowButton>
+    </div>
+  );
+}
+
+export function UnfollowButton() {
+  return (
+    <div>
+      <S.UnfollowButton>언팔로우</S.UnfollowButton>
+    </div>
+  );
+}
+
+export function SaveButton() {
+  return (
+    <div>
+      <S.SaveButton>저장</S.SaveButton>
+    </div>
+  );
+}
+
+export function DisabledSaveButton() {
+  return (
+    <div>
+      <S.DisabledSaveButton>저장</S.DisabledSaveButton>
+    </div>
+  );
+}
+
+export function SmallFollowButton() {
+  return (
+    <div>
+      <S.SmallFollowButton>팔로우</S.SmallFollowButton>
+    </div>
+  );
+}
+export function CancelButton() {
+  return (
+    <div>
+      <S.CancelButton>취소</S.CancelButton>
+    </div>
+  );
+}
---
+++ src/components/common/button/button.style.js
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+
+export const NextButton = styled.button`
+  background: #55bdb3;
+  color: #fff;
+  border-radius: 44px;
+  padding: 13px 148px 12px 148px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 19px;
+  width: 322px;
+  height: 44px;
+`;
+
+export const DisabledNextButton = styled.button`
+  background: #d9d9d9;
+  color: #fff;
+  border-radius: 44px;
+  padding: 13px 148px 12px 148px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 19px;
+  width: 322px;
+  height: 44px;
+`;
+
+export const MediumFollowButton = styled.button`
+  background: #55bdb3;
+  color: #fff;
+  border-radius: 30px;
+  padding: 7px 40px 8px 41px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 19px;
+  width: 120px
+  height: 34px;
+`;
+
+export const DisabledFollowButton = styled.button`
+  background: #d9d9d9;
+  color: #fff;
+  border-radius: 30px;
+  padding: 7px 40px 8px 41px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 19px;
+  width: 120px;
+  height: 34px;
+`;
+
+export const UnfollowButton = styled.button`
+  background: #fff;
+  color: #767676;
+  border: 1px solid #dbdbdb;
+  border-radius: 30px;
+  box-sizing: border-box;
+  padding: 7px 34px 8px 34px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 19px;
+  width: 120px;
+  height: 34px;
+`;
+
+export const SaveButton = styled.button`
+  background: #55bdb3;
+  color: #fff;
+  border-radius: 32px;
+  padding: 6px 33px 7px 31px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 19px;
+`;
+
+export const DisabledSaveButton = styled.button`
+  background: #d9d9d9;
+  color: #fff;
+  border-radius: 32px;
+  padding: 6px 33px 7px 31px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 19px;
+`;
+
+export const SmallFollowButton = styled.button`
+  background: #55bdb3;
+  color: #fff;
+  border-radius: 26px;
+  padding: 7px 11px;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 15px;
+  width: 56px;
+  height: 28px;
+`;
+
+export const CancelButton = styled.button`
+  background: #fff;
+  color: #767676;
+  border: 1px solid #dbdbdb;
+  border-radius: 26px;
+  padding: 7px 11px;
+  box-sizing: border-box;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 15px;
+  width: 56px;
+  height: 28px;
+`;

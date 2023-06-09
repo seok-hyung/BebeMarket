5457ba394941fb181570cbb17a90ff35c34aa946
Seokhyung Lee <123619657+seok-hyung@users.noreply.github.com>
2023-06-09 15:02:43 +0900

    Update button.jsx

--- src/components/common/button/button.jsx
+++ src/components/common/button/button.jsx
@@ -2,72 +2,37 @@ import React from 'react';
 import * as S from './Button.style.js';
 
 export function NextButton() {
-  return (
-    <div>
-      <S.NextButton>다음</S.NextButton>
-    </div>
-  );
+  return <S.NextButton>다음</S.NextButton>;
 }
 
 export function DisabledNextButton() {
-  return (
-    <div>
-      <S.DisabledNextButton>다음</S.DisabledNextButton>
-    </div>
-  );
+  return <S.DisabledNextButton>다음</S.DisabledNextButton>;
 }
 
 export function MediumFollowButton() {
-  return (
-    <div>
-      <S.MediumFollowButton>팔로우</S.MediumFollowButton>
-    </div>
-  );
+  return <S.MediumFollowButton>팔로우</S.MediumFollowButton>;
 }
 
 export function DisabledFollowButton() {
-  return (
-    <div>
-      <S.DisabledFollowButton>팔로우</S.DisabledFollowButton>
-    </div>
-  );
+  return <S.DisabledFollowButton>팔로우</S.DisabledFollowButton>;
 }
 
 export function UnfollowButton() {
-  return (
-    <div>
-      <S.UnfollowButton>언팔로우</S.UnfollowButton>
-    </div>
-  );
+  return <S.UnfollowButton>언팔로우</S.UnfollowButton>;
 }
 
 export function SaveButton() {
-  return (
-    <div>
-      <S.SaveButton>저장</S.SaveButton>
-    </div>
-  );
+  return <S.SaveButton>저장</S.SaveButton>;
 }
 
 export function DisabledSaveButton() {
-  return (
-    <div>
-      <S.DisabledSaveButton>저장</S.DisabledSaveButton>
-    </div>
-  );
+  return <S.DisabledSaveButton>저장</S.DisabledSaveButton>;
 }
 
 export function SmallFollowButton() {
-  return (
-    <div>
-      <S.SmallFollowButton>팔로우</S.SmallFollowButton>
-    </div>
-  );
+  return <S.SmallFollowButton>팔로우</S.SmallFollowButton>;
 }
+
 export function CancelButton() {
-  return (
-    <div>
-      <S.CancelButton>취소</S.CancelButton>
-    </div>
-  );
+  return <S.CancelButton>취소</S.CancelButton>;
 }

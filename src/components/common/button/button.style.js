93b1ec270827322cdaeb0e9e5a24ca35492d5e45
DESKTOP-KRJ342G\korea <korea970223@naver.com>
2023-06-12 15:14:59 +0900

    :lipstick:Modify : 버튼의 padding 값 삭제

--- src/components/common/button/button.style.js
+++ src/components/common/button/button.style.js
@@ -4,7 +4,6 @@ export const NextButton = styled.button`
   background: #55bdb3;
   color: #fff;
   border-radius: 44px;
-  padding: 13px 148px 12px 148px;
   font-size: 14px;
   font-weight: 500;
   line-height: 19px;
@@ -16,7 +15,6 @@ export const DisabledNextButton = styled.button`
   background: #d9d9d9;
   color: #fff;
   border-radius: 44px;
-  padding: 13px 148px 12px 148px;
   font-size: 14px;
   font-weight: 500;
   line-height: 19px;
@@ -28,7 +26,6 @@ export const MediumFollowButton = styled.button`
   background: #55bdb3;
   color: #fff;
   border-radius: 30px;
-  padding: 7px 40px 8px 41px;
   font-size: 14px;
   font-weight: 500;
   line-height: 19px;
@@ -40,7 +37,6 @@ export const DisabledFollowButton = styled.button`
   background: #d9d9d9;
   color: #fff;
   border-radius: 30px;
-  padding: 7px 40px 8px 41px;
   font-size: 14px;
   font-weight: 500;
   line-height: 19px;
@@ -54,7 +50,6 @@ export const UnfollowButton = styled.button`
   border: 1px solid #dbdbdb;
   border-radius: 30px;
   box-sizing: border-box;
-  padding: 7px 34px 8px 34px;
   font-size: 14px;
   font-weight: 500;
   line-height: 19px;
@@ -66,7 +61,6 @@ export const SaveButton = styled.button`
   background: #55bdb3;
   color: #fff;
   border-radius: 32px;
-  padding: 6px 33px 7px 31px;
   font-size: 14px;
   font-weight: 500;
   line-height: 19px;
@@ -76,7 +70,6 @@ export const DisabledSaveButton = styled.button`
   background: #d9d9d9;
   color: #fff;
   border-radius: 32px;
-  padding: 6px 33px 7px 31px;
   font-size: 14px;
   font-weight: 500;
   line-height: 19px;
@@ -86,7 +79,6 @@ export const SmallFollowButton = styled.button`
   background: #55bdb3;
   color: #fff;
   border-radius: 26px;
-  padding: 7px 11px;
   font-size: 12px;
   font-weight: 400;
   line-height: 15px;
@@ -99,7 +91,6 @@ export const CancelButton = styled.button`
   color: #767676;
   border: 1px solid #dbdbdb;
   border-radius: 26px;
-  padding: 7px 11px;
   box-sizing: border-box;
   font-size: 12px;
   font-weight: 400;

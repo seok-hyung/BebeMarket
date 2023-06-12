a385100b4266ed4ff1bc1c94779b1d12e6586777
sujung-lim <sujunglim94@gmail.com>
2023-06-13 06:02:15 +0900

    ✨ Feat: Profile 페이지 프레임 초안 #25

---
+++ src/pages/profile/Profile.style.js
@@ -0,0 +1,126 @@
+import styled from 'styled-components';
+
+export const ProfileContainer = styled.div`
+  height: auto;
+  width: 100%;
+`;
+
+// 프로필 header
+export const ProfileHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 41px;
+  margin-top: 30px;
+  margin-bottom: 14px;
+`;
+
+export const Followers = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ProfileImage = styled.img`
+  width: 110px;
+  height: 110px;
+`;
+
+export const Followings = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+// 프로필 main
+export const ProfileMain = styled.div`
+  text-align: center;
+
+  & p:first-child {
+    font-weight: 700;
+    line-height: 22px;
+  }
+
+  & p:nth-child(2) {
+    font-size: 12px;
+    line-height: 14px;
+    margin-top: 6px;
+    color: var(--sub-text-color);
+  }
+
+  & p:nth-child(3) {
+    font-size: 14px;
+    line-height: 18px;
+    margin-top: 16px;
+    color: var(--sub-text-color);
+  }
+`;
+
+// 프로필 footer
+export const ProfileFooter = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin-top: 24px;
+  margin-bottom: 26px;
+
+  // DM 메시지 버튼
+  .dm-btn {
+    border: 1px solid #dbdbdb;
+    border-radius: 30px;
+    background: url(../../../assets/icon/icon-message-circle-1.svg) no-repeat;
+    background-size: 15px 15px;
+    width: 34px;
+    height: 34px;
+  }
+
+  // 공유하기 버튼
+  .share-btn {
+    border: 1px solid #dbdbdb;
+    border-radius: 30px;
+    background: url(../assets/icon/icon-share.svg);
+    width: 34px;
+    height: 34px;
+  }
+`;
+
+// 상품 product
+export const ProductContainer = styled.div`
+  margin-top: 6px;
+  margin: auto;
+  width: 390px;
+
+  h2 {
+    padding: 18px 0 0 21px;
+  }
+`;
+
+// post container
+export const PostContainer = styled.div`
+  height: 553px;
+`;
+
+// 피드 정렬
+export const ViewOptions = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 22.5px;
+  border-bottom: 1px solid #dbdbdb;
+  margin: auto;
+  padding-right: 12.25px;
+  width: 390px;
+  height: 44px;
+`;
+
+export const PostListOn = styled.img`
+  cursor: pointer;
+  width: 26px;
+  height: 26px;
+`;
+
+export const PostAlbumOff = styled.img`
+  cursor: pointer;
+  width: 26px;
+  height: 26px;
+`;

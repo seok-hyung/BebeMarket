93a14f77881b4070ad5460d830007e953293cd45
sujung-lim <sujunglim94@gmail.com>
2023-06-14 01:18:48 +0900

    ✨ Modify: 앨범 아이콘 클릭시 피드 목록형 -> 앨범형으로 변경 코드 수정

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 // 공통 컴포넌트
 import TopBasicNav from '../../components/common/topNav/TopBasicNav';
-import UnfollowButton from '../../components/common/button/button';
+import { MediumFollowButton } from '../../components/common/button/button';
 import Product from '../../components/common/product/Product';
-import HomePostList from '../../components/common/home/HomePost';
+import HomePost from '../../components/common/home/HomePost';
+import HomeAlbum from '../home/HomeAlbum';
 import TabMenu from '../../components/common/tab/TabMenu';
 
 // 스타일
@@ -20,50 +21,50 @@ export default function Profile() {
   return (
     <div>
       <TopBasicNav />
-      <S.ProfileContainer>
-        <S.ProfileHeader>
-          <S.Followers>
-            <span>2950</span>
-            <span>followers</span>
-          </S.Followers>
-          <S.ProfileImage src={basicProfileImage} alt="프로필 사진" />
-          <S.Followings>
-            <span>128</span>
-            <span>followings</span>
-          </S.Followings>
-        </S.ProfileHeader>
-        <S.ProfileMain>
-          <p>애월읍 위니브 감귤농장</p>
-          <p>@ weniv_Mandarin</p>
-          <p>애월읍 감귤 전국 배송, 귤따기 체험, 감귤 농장</p>
-        </S.ProfileMain>
-        <S.ProfileFooter>
-          <button className="dm-btn"></button>
-          <UnfollowButton />
-          <button className="share-btn"></button>
-        </S.ProfileFooter>
-      </S.ProfileContainer>
-      <S.ProductContainer>
-        <h2>판매 중인 상품</h2>
-        <Product />
-      </S.ProductContainer>
-      <S.PostContainer>
-        <S.ViewOptions>
-          <S.PostListOn
-            src={postListOn}
-            alt="피드 목록형으로 보기"
-            onClick={() => setIsListMode(true)}
-          ></S.PostListOn>
-          <S.PostAlbumOff
-            src={postAlbumOff}
-            alt="피드 앨범형으로 보기"
-            onClick={() => setIsListMode(false)}
-          ></S.PostAlbumOff>
-        </S.ViewOptions>
-        <div style={isListMode ? {} : { display: 'none' }}>
-          <HomePostList /> {/* 목록형 레이아웃 */}
-        </div>
-      </S.PostContainer>
+      <S.Container>
+        <S.ProfileContainer>
+          <S.ProfileHeader>
+            <S.Followers>
+              <span>2950</span>
+              <span>followers</span>
+            </S.Followers>
+            <S.ProfileImage src={basicProfileImage} alt="프로필 사진" />
+            <S.Followings>
+              <span>128</span>
+              <span>followings</span>
+            </S.Followings>
+          </S.ProfileHeader>
+          <S.ProfileMain>
+            <p>애월읍 위니브 감귤농장</p>
+            <p>@ weniv_Mandarin</p>
+            <p>애월읍 감귤 전국 배송, 귤따기 체험, 감귤 농장</p>
+          </S.ProfileMain>
+          <S.ProfileFooter>
+            <button className="dm-btn"></button>
+            <MediumFollowButton />
+            <button className="share-btn"></button>
+          </S.ProfileFooter>
+        </S.ProfileContainer>
+        <S.ProductContainer>
+          <h2>판매 중인 상품</h2>
+          <Product />
+        </S.ProductContainer>
+        <S.PostContainer>
+          <S.ViewOptions>
+            <S.PostListOn
+              src={postListOn}
+              alt="피드 목록형으로 보기"
+              onClick={() => setIsListMode(true)}
+            ></S.PostListOn>
+            <S.PostAlbumOff
+              src={postAlbumOff}
+              alt="피드 앨범형으로 보기"
+              onClick={() => setIsListMode(false)}
+            ></S.PostAlbumOff>
+          </S.ViewOptions>
+          {isListMode ? <HomePost /> : <HomeAlbum />}
+        </S.PostContainer>
+      </S.Container>
       <TabMenu />
     </div>
   );

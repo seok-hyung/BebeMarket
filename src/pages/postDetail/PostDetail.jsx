a8774ce8ae85db63b1b6ca829a139c8c38411ae4
susuje <wjdtn0n@naver.com>
2023-06-13 17:15:36 +0900

    :sparkles:Feat : postDetail 페이지 마크업 #33

---
+++ src/pages/postDetail/PostDetail.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as S from './PostDetail.style';
+import TopBasicNav from '../../components/common/topNav/TopBasicNav';
+import HomePost from '../../components/common/home/HomePost';
+import CommentInput from '../../components/common/user/Comment';
+import PostComment from '../../components/postComment/PostComment';
+export default function PostDetail() {
+  return (
+    <S.Container>
+      <TopBasicNav />
+      <HomePost />
+      <S.Line />
+      <S.CommentBox>
+        <PostComment />
+      </S.CommentBox>
+      <CommentInput />
+    </S.Container>
+  );
+}

63d5e17fb37cf1874f2858c7ed9dc5fbd5e3efb9
susuje <wjdtn0n@naver.com>
2023-06-14 15:56:05 +0900

    :zap:Modify : topNav 코드 수정#45

--- src/components/common/topNav/TopMainNav.jsx
+++ src/components/common/topNav/TopMainNav.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import * as S from './TopMainNav.style.js';
 import searchIcon from '../../../assets/icon/icon-search.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function TopMainNav() {
+  const navigate = useNavigate();
   return (
     <S.Nav>
       <S.Title>감귤마켓 피드</S.Title>
-      <S.Button>
+      <S.Button onClick={() => navigate('/search')}>
         <img src={searchIcon} alt="검색 아이콘" />
       </S.Button>
     </S.Nav>

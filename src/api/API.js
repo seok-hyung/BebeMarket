52f902042172c1385d40caf0a7c3148f234e3ac5
Seokhyung Lee <korea970223@naver.com>
2023-06-15 13:57:23 +0900

    :sparkles:Feat : 회원가입, 이메일검증, 계정 검증, 이미지 업로드 구현 #64

--- src/api/API.js
+++ src/api/API.js
@@ -64,3 +64,57 @@ export const accountnameValidAPI = async (userID) => {
 };
 
 // 한 개의 이미지 업로드(프로필,상품)
+export const uploadImageAPI = async (image) => {
+  try {
+    const formData = new FormData();
+    formData.append('image', image);
+    const res = await fetch(
+      'https://mandarin.api.weniv.co.kr/image/uploadfile',
+      {
+        method: 'POST',
+        body: formData,
+      },
+    );
+    const json = await res.json();
+    if (json.message) {
+      // eslint-disable-next-line no-alert
+      alert(`${json.message}`);
+    }
+    return `https://mandarin.api.weniv.co.kr/${json.filename}`;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+//이메일 로그인
+export const emailLoginAPI = async (email, password) => {
+  try {
+    const response = await axios.post(
+      'https://mandarin.api.weniv.co.kr/user/login',
+      {
+        user: {
+          email,
+          password,
+        },
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    //  undefined 일 때 대비하는 로직
+    if (response.data) {
+      console.log(response);
+      console.log(response.data);
+      return response.data;
+    } else {
+      console.log('응답 데이터가 없습니다.');
+      return null;
+    }
+  } catch (error) {
+    console.error('API 요청 오류:', error.message);
+    throw new Error(error.message);
+  }
+};

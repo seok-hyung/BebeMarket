fa2a19797b8bc5ad5fedc2aa83d0cc4d98397846
sujung-lim <sujunglim94@gmail.com>
2023-06-15 23:19:31 +0900

    ✨Feat: input 값이 모두 채워지면 저장 버튼 활성화 기능 추가

--- src/pages/profileEdit/ProfileEdit.jsx
+++ src/pages/profileEdit/ProfileEdit.jsx
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TopUploadNav from '../../components/common/topNav/TopUploadNav';
 import * as S from './ProfileEdit.style';
 import ProfileImage from '../../assets/images/full-logo.svg';
 
 export default function ProfileEdit() {
+  //인풋 입력 상태 저장
+  const [username, setUsername] = useState(''); //사용자 이름 값
+  const [accountId, setAccountId] = useState(''); // 계정 ID 값
+  const [introduction, setIntroduction] = useState(''); // 소개 값
+
+  // 모든 input 값이 채워졌는지 확인
+  const [isInputFilled, setIsInputFilled] = useState(false);
+
+  useEffect(() => {
+    if (username && accountId && introduction) {
+      setIsInputFilled(true);
+    } else {
+      setIsInputFilled(false);
+    }
+  }, [username, accountId, introduction]);
+
   return (
     <div>
       <TopUploadNav>
-        <S.CustomDisabledSaveButton>저장</S.CustomDisabledSaveButton>
+        {isInputFilled ? (
+          <S.CustomSaveButton>저장</S.CustomSaveButton>
+        ) : (
+          <S.CustomDisabledSaveButton>저장</S.CustomDisabledSaveButton>
+        )}
       </TopUploadNav>
       <S.ProfileEdit>
         <S.ProfileImage src={ProfileImage} />
         <S.InputWrapper>
           <p>사용자 이름</p>
-          <input type="text" placeholder="2~10자 이내여야 합니다." />
+          <input
+            type="text"
+            placeholder="2~10자 이내여야 합니다."
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </S.InputWrapper>
         <S.InputWrapper>
           <p>계정 ID</p>
           <input
             type="text"
             placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
+            value={accountId}
+            onChange={(e) => setAccountId(e.target.value)}
           />
         </S.InputWrapper>
         <S.InputWrapper>
@@ -27,6 +54,8 @@ export default function ProfileEdit() {
           <input
             type=" "
             placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
+            value={introduction}
+            onChange={(e) => setIntroduction(e.target.value)}
           />
         </S.InputWrapper>
       </S.ProfileEdit>

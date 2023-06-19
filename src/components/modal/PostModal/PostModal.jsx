ed7df4cace644b48a00235031487dc85f89b8633
susuje <wjdtn0n@naver.com>
2023-06-19 16:51:43 +0900

    :lipstick:Modify : PostModal 코드 수정 #33

--- src/components/modal/PostModal/PostModal.jsx
+++ src/components/modal/PostModal/PostModal.jsx
@@ -3,7 +3,7 @@ import { Container, Ul, Li, GrayLine, Button } from '../styled';
 import PostAlert from './PostAlert';
 import { useNavigate } from 'react-router-dom';
 
-export default function PostModal({ setisModalOpen }) {
+export default function PostModal({ setisModalOpen, isMyPost }) {
   const [isAlertOpen, setIsAlertOpen] = useState(false);
   const navigate = useNavigate();
   const handleToEdit = () => {
@@ -14,15 +14,20 @@ export default function PostModal({ setisModalOpen }) {
       <Container onClick={() => setisModalOpen(false)}></Container>
       <Ul>
         <GrayLine></GrayLine>
-        <Li>
-          <Button onClick={() => setIsAlertOpen(true)}>삭제</Button>
-        </Li>
-        <Li>
-          <Button onClick={handleToEdit}>수정</Button>
-        </Li>
-        <Li>
-          <Button>신고</Button>
-        </Li>
+        {isMyPost ? (
+          <>
+            <Li>
+              <Button onClick={() => setIsAlertOpen(true)}>삭제</Button>
+            </Li>
+            <Li>
+              <Button onClick={handleToEdit}>수정</Button>
+            </Li>
+          </>
+        ) : (
+          <Li>
+            <Button>신고</Button>
+          </Li>
+        )}
       </Ul>
       {isAlertOpen ? <PostAlert setIsAlertOpen={setIsAlertOpen} /> : null}
     </>
@@ -30,3 +35,13 @@ export default function PostModal({ setisModalOpen }) {
 }
 //수정해야할것
 //내 포스트면 삭제와 수정 Li가 떠야하고, 남의 포스트면 신고Li만 떠야한다.
+
+/* <Li>
+<Button onClick={() => setIsAlertOpen(true)}>삭제</Button>
+</Li>
+<Li>
+<Button onClick={handleToEdit}>수정</Button>
+</Li>
+<Li>
+<Button>신고</Button>
+</Li> */

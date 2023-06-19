83740ce4163a5834590010e21f1ee2e82439bd01
Seokhyung Lee <korea970223@naver.com>
2023-06-19 22:07:34 +0900

    :truck:Modify : api 주소 수정

--- src/api/profile/getFollowerAPI.js
+++ src/api/profile/getFollowerAPI.js
@@ -3,15 +3,12 @@ import { apiURL } from '../apiURL';
 
 const getFollowerAPI = async (token) => {
   try {
-    const response = await axios.get(
-      `${apiURL}post/feed/?limit=Number&skip=Number`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-type': 'application/json',
-        },
+    const response = await axios.get(`${apiURL}post/feed`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-type': 'application/json',
       },
-    );
+    });
     return response.data.posts;
   } catch (error) {
     console.log('팔로잉 게시글 목록 API 요청 오류', error);

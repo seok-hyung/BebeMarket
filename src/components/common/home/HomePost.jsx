9f240b2e5970f01ab89e78421dcbea70cb8576a3
susuje <wjdtn0n@naver.com>
2023-06-19 16:51:01 +0900

    :lipstick:Modify : homePost 수정 #33

--- src/components/common/home/HomePost.jsx
+++ src/components/common/home/HomePost.jsx
@@ -1,49 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './HomePost.style';
 import PostModal from '../../modal/PostModal/PostModal';
+import { useRecoilValue } from 'recoil';
+import { accountNameState } from '../../../atoms/Atoms';
 
-function HomePost() {
+function HomePost({ post }) {
   const [isModalOpen, setisModalOpen] = useState(false);
+  const [isMyPost, setIsMyPost] = useState(false);
   const showModal = () => {
     setisModalOpen(true);
   };
+  const accountname = useRecoilValue(accountNameState);
+
+  useEffect(() => {
+    if (post.author.accountname === accountname) {
+      setIsMyPost(true);
+    }
+  }, [post.author.accountname, accountname]);
+
   return (
     <>
       <S.Wrapper>
         <S.Article>
-          <S.UserImage></S.UserImage>
+          <S.UserImage src={post.author.image} />
           <S.PostWapper>
             <S.PostHeader>
               <S.Title>
-                애월읍 위니브 감귤농장
-                <S.SubTitle>@ weniv_Mandarin</S.SubTitle>
+                {post.author.username}
+                <S.SubTitle>{post.author.accountname}</S.SubTitle>
               </S.Title>
               <S.MoreButton onClick={showModal}></S.MoreButton>
             </S.PostHeader>
             <S.PostDetail>
-              <S.Content>
-                옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여,
-                뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할
-                넣는 풍부하게 뛰노는 인생의 힘있다.
-              </S.Content>
-              <S.ContentImage></S.ContentImage>
+              <S.Content>{post.content}</S.Content>
+              {post.image.split(',').length === 1 ? (
+                <S.ContentImage src={post.image.split(',')[0]} />
+              ) : (
+                <S.MultipleImgUl>
+                  {post.image.split(',').map((image, index) => (
+                    <S.ContentImageWrapper key={index}>
+                      <S.ContentImage src={image} alt="포스트 이미지" />
+                    </S.ContentImageWrapper>
+                  ))}
+                </S.MultipleImgUl>
+              )}
             </S.PostDetail>
             <S.PostIconWrapper>
               <S.Like>
                 <S.LikeIcon></S.LikeIcon>
-                <S.LikeCount>58</S.LikeCount>
+                <S.LikeCount>{post.heartCount}</S.LikeCount>
               </S.Like>
               <S.Comment>
                 <S.CommentIcon></S.CommentIcon>
-                <S.CommentCount>12</S.CommentCount>
+                <S.CommentCount>{post.commentCount}</S.CommentCount>
               </S.Comment>
             </S.PostIconWrapper>
-            <S.PostDate>2020년 10월 21일</S.PostDate>
+            <S.PostDate>{post.createdAt}</S.PostDate>
           </S.PostWapper>
         </S.Article>
       </S.Wrapper>
-      {isModalOpen ? <PostModal setisModalOpen={setisModalOpen} /> : null}
+      {isModalOpen ? (
+        <PostModal setisModalOpen={setisModalOpen} isMyPost={isMyPost} />
+      ) : null}
     </>
   );
 }
 export default HomePost;
+
+// {post.image.split(',').length === 1 ? (
+//   <S.ContentImage src={post.image.split(',')[0]} />
+// ) : (
+//   <S.MultipleImgUl>
+//     <S.ContentImageWrapper>
+//       <S.ContentImage src={post.image.split(',')[0]} />
+//     </S.ContentImageWrapper>
+//     <S.ContentImageWrapper>
+//       <S.ContentImage src={post.image.split(',')[1]} />
+//     </S.ContentImageWrapper>
+//   </S.MultipleImgUl>
+// )}
+
+// if (post.author.accountname === accountname) {
+//   setIsMyPost(true);
+// } 라고 했을때는 isMyPost가 재렌더링 되어서 무한루프가 일어난다.

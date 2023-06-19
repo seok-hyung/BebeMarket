80833ff55a23d964eaa7eb69082fbd052ead753a
Seokhyung Lee <korea970223@naver.com>
2023-06-20 02:01:26 +0900

    :pencil2:Modify : return값 변경

--- src/api/profile/getFollowerAPI.js
+++ src/api/profile/getFollowerAPI.js
@@ -9,7 +9,7 @@ const getFollowerAPI = async (token) => {
         'Content-type': 'application/json',
       },
     });
-    return response.data.posts;
+    return response.data;
   } catch (error) {
     console.log('팔로잉 게시글 목록 API 요청 오류', error);
     return null;

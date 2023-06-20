866385eb8b6a37181b00c564ba6c3e72d9cbf9d5
seokhyungth <korea970223@naver.com>
2023-06-20 19:21:37 +0900

    :truck:Modify : apiURL 추가 #84

--- src/api/product/addProductAPI.js
+++ src/api/product/addProductAPI.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
+import { apiURL } from '../apiURL';
 
 export const addProductAPI = async (sendData, userToken) => {
   try {
-    const response = await axios.post(
-      'https://api.mandarin.weniv.co.kr/product',
-      sendData,
-      {
-        headers: {
-          Authorization: `Bearer ${userToken}`,
-          'Content-type': 'application/json',
-        },
+    const response = await axios.post(`${apiURL}product`, sendData, {
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        'Content-type': 'application/json',
       },
-    );
+    });
     return response.data;
   } catch (error) {
     console.error('Error in addProductAPI:', error.message);

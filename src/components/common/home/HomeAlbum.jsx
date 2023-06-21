3ae06397840de5a39718f6bd4d8511570d3979da
sujung-lim <sujunglim94@gmail.com>
2023-06-21 16:26:21 +0900

    :lipstick:Style : 게시물 이미지가 여러장일 경우 게시물 위 아이콘 표시

--- src/components/common/home/HomeAlbum.jsx
+++ src/components/common/home/HomeAlbum.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import { HomeAlbumContainer, HomeAlbumItem } from './HomeAlbum.style';
+import LayerImage from '../../../assets/icon/iccon-img-layers.svg';
 
-const HomeAlbum = ({ image }) => {
+const HomeAlbum = ({ profileArray }) => {
+  console.log(profileArray);
   return (
     <HomeAlbumContainer>
-      <HomeAlbumItem>
-        <img src={image} alt="앨범형 이미지" />
-      </HomeAlbumItem>
+      {profileArray.map((item, index) => {
+        const images = item.image.split(',');
+
+        return (
+          <HomeAlbumItem key={index}>
+            {images.length > 1 ? (
+              <div
+                style={{ position: 'relative', width: '100%', height: '100%' }}
+              >
+                <img src={images[0]} alt="앨범형 이미지" />
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: '0',
+                    right: '0',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <img src={LayerImage} alt="아이콘" />
+                </div>
+              </div>
+            ) : (
+              <img src={item.image} alt="앨범형 이미지" />
+            )}
+          </HomeAlbumItem>
+        );
+      })}
     </HomeAlbumContainer>
   );
 };

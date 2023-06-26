773e6980a7c4200b7f84cddb810f94566098c683
seokhyungth <korea970223@naver.com>
2023-06-26 14:55:56 +0900

    :sparkles:Feat : 계정ID 중복검사 구현

--- src/pages/profileEdit/ProfileEdit.jsx
+++ src/pages/profileEdit/ProfileEdit.jsx
@@ -9,6 +9,7 @@ import * as S from './ProfileEdit.style';
 import InputBox from '../../components/common/input/InputBox';
 import { getMyInfoAPI } from '../../api/user/getMyInfoAPI';
 import { uploadImageAPI } from '../../api/uploadImg/uploadImageAPI';
+import { accountnameValidAPI } from '../../api/user/accountnameValidAPI';
 
 export default function ProfileEdit() {
   const navigate = useNavigate();
@@ -23,7 +24,7 @@ export default function ProfileEdit() {
   const [isInputFilled, setIsInputFilled] = useState(false);
 
   // 유효성 검사 결과 저장
-  const [isUsernameValid, setIsUsernameValid] = useState(false);
+  const [isUsernameValid, setIsUsernameValid] = useState(true);
   const [isAccountIdValid, setIsAccountIdValid] = useState(true);
 
   // 유효성 검사 통과 실패 시 에러 메시지
@@ -77,6 +78,20 @@ export default function ProfileEdit() {
     } else {
       setIsUsernameValid(false);
       setUsernameErrorMessage('* 2~10자로 작성해주세요');
+    }
+  };
+
+  // userId 중복 검사 (api)
+  const handleUserIdDuplicateValid = async (e) => {
+    const testAccountname = e.target.value;
+    setAccountId(testAccountname);
+    const validMessage = await accountnameValidAPI(testAccountname);
+    if (validMessage?.message === '이미 가입된 계정ID 입니다.') {
+      setIsAccountIdValid(false);
+      setAccountIdErrorMessage('* 이미 사용 중인 ID입니다.');
+    } else {
+      setIsAccountIdValid(true);
+      setAccountIdErrorMessage('');
     }
   };
 
@@ -154,7 +169,7 @@ export default function ProfileEdit() {
           max={10}
           borderBottomColor={isUsernameValid ? null : 'on'}
           errorMessage={usernameErrorMessage}
-          show={!isUsernameValid ? 'on' : null}
+          show={isUsernameValid ? null : 'on'}
           onChange={handleUserNameValid}
         />
         <InputBox
@@ -167,6 +182,7 @@ export default function ProfileEdit() {
           errorMessage={accountIdErrorMessage}
           show={!isAccountIdValid ? 'on' : null}
           onChange={handleAccountIdValid}
+          onInput={handleUserIdDuplicateValid}
         />
         <InputBox
           label="소개"

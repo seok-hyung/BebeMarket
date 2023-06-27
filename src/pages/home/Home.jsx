63c5de4ad23583718406feac88196a93dd4dd316
seokhyungth <korea970223@naver.com>
2023-06-27 11:39:20 +0900

    :sparkles:Feat : 홈페이지 무한스크롤 구현 #112

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import * as S from './Home.style';
 import Image from '../../assets/images/symbol-logo-gray.svg';
 import loadingGif from '../../assets/images/loading.gif';
@@ -17,21 +17,50 @@ function Home() {
   const userToken = useRecoilValue(userTokenState);
   const [followerData, setFollowerData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [skip, setSkip] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+
+  const loadMorePosts = useCallback(async () => {
+    setIsLoading(true);
+    const data = await getPostFeedAPI(userToken, skip, 4);
+    if (data) {
+      setFollowerData((prevState) => [...prevState, ...data]);
+      if (data.length < 4) {
+        setHasMore(false);
+      } else {
+        setSkip((prevState) => prevState + 4);
+      }
+    }
+    setIsLoading(false);
+  }, [skip, userToken]);
 
   useEffect(() => {
-    setIsLoading(true);
-    getPostFeedAPI(userToken).then((data) => {
-      setFollowerData(data.posts);
-      setIsLoading(false);
-    });
+    loadMorePosts();
   }, []);
+
+  const handleScroll = useCallback(() => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight - 50
+    ) {
+      if (!isLoading && hasMore) {
+        loadMorePosts();
+      }
+    }
+  }, [isLoading, hasMore, loadMorePosts]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
 
   return (
     <>
       <TopMainNav />
       <S.HomeWrapper>
         {isLoading ? (
-          // 로딩 중일 때 표시할 내용을 조건부 렌더링합니다.
           <S.LoadingImage src={loadingGif} alt="로딩중일 때 이미지" />
         ) : followerData?.length > 0 ? (
           followerData.map((post) => (

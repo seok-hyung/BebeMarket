dc4b58f4a3347389600ed638330d7547300f64d1
susuje <wjdtn0n@naver.com>
2023-07-11 20:18:55 +0900

    :lipstick:Modi : tag 기능 추가 #139

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,23 +8,63 @@ import TabMenu from '../../components/common/tab/TabMenu';
 import { useNavigate } from 'react-router-dom';
 
 import { useRecoilValue } from 'recoil';
-import { userTokenState } from '../../atoms/Atoms';
+import { userTokenState, accountNameState } from '../../atoms/Atoms';
 import HomePost from '../../components/common/home/HomePost';
 import getPostFeedAPI from '../../api/post/getPostFeedAPI';
+import getAllFeedAPI from '../../api/post/getAllFeedAPI';
 
 function Home() {
   const navigate = useNavigate();
   const userToken = useRecoilValue(userTokenState);
-  const [followerData, setFollowerData] = useState([]);
+  const userAccountname = useRecoilValue(accountNameState);
+  const [feedData, setFeedData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [skip, setSkip] = useState(0);
   const [hasMore, setHasMore] = useState(true);
+  const [selectedTag, setSelectedTag] = useState('팔로잉');
+
+  const handleClickTag = async (tag) => {
+    setSelectedTag(tag);
+    if (tag === '팔로잉') {
+      loadFollowingPosts();
+    } else {
+      const data = await getAllFeedAPI(userToken);
+      const allFeedArr = data.posts;
+
+      const regex = new RegExp(`\\\\tag:.*${tag}.*(?=,|\\\\|$)`);
+      const TaggeddArr = allFeedArr.filter((item) =>
+        item.author.accountname === userAccountname
+          ? null
+          : item.content.match(regex),
+      );
+      console.log(TaggeddArr);
+      setFeedData(TaggeddArr);
+    }
+  };
+  const Tags = [
+    '팔로잉',
+    '육아',
+    '일상',
+    '요리',
+    '꿀팁',
+    '음식점',
+    '꿀템추천',
+    '키즈카페',
+  ];
+  const loadFollowingPosts = useCallback(async () => {
+    setIsLoading(true);
+    const data = await getPostFeedAPI(userToken, 0, Infinity); // 모든 글을 불러오기 위해 skip을 설정하지 않고, Infinity를 넘겨줍니다.
+    if (data) {
+      setFeedData(data);
+    }
+    setIsLoading(false);
+  }, [userToken]);
 
   const loadMorePosts = useCallback(async () => {
     setIsLoading(true);
     const data = await getPostFeedAPI(userToken, skip, 4);
     if (data) {
-      setFollowerData((prevState) => [...prevState, ...data]);
+      setFeedData((prevState) => [...prevState, ...data]);
       if (data.length < 4) {
         setHasMore(false);
       } else {
@@ -59,15 +99,28 @@ function Home() {
   return (
     <>
       <TopMainNav />
+      <S.TagWrapper>
+        {Tags.map((tag, index) => (
+          <S.TagList
+            type="button"
+            key={index}
+            value={tag}
+            onClick={() => handleClickTag(tag)}
+            clicked={tag === selectedTag}
+          >
+            # {tag}
+          </S.TagList>
+        ))}
+      </S.TagWrapper>
       <S.HomeWrapper>
         {isLoading ? (
           <S.LoadingImage src={loadingGif} alt="로딩중일 때 이미지" />
-        ) : followerData?.length > 0 ? (
-          followerData.map((post) => (
+        ) : feedData?.length > 0 ? (
+          feedData.map((post) => (
             <HomePost key={post.id} post={post} postId={post.id} />
           ))
         ) : (
-          <>
+          <S.Div>
             <S.HomeDiv>
               <S.HomeImage src={Image} alt="홈 피드 내용이 없을 때의 이미지" />
               <S.HomeDescription>
@@ -77,7 +130,7 @@ function Home() {
                 검색하기
               </S.SearchButton>
             </S.HomeDiv>
-          </>
+          </S.Div>
         )}
       </S.HomeWrapper>
       <TabMenu />
--- src/pages/home/Home.style.js
+++ src/pages/home/Home.style.js
@@ -4,10 +4,10 @@ export const HomeWrapper = styled.div`
   min-height: calc(100vh - 108px); // 헤더 + 네브 === 108
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  //justify-content: center;
   align-items: center;
   margin-bottom: 60px;
-  margin-top: 50px;
+  //z-index: 5;
 `;
 
 export const LoadingImage = styled.img`
@@ -16,13 +16,15 @@ export const LoadingImage = styled.img`
   justify-content: center;
   align-items: center;
 `;
-
+export const Div = styled.div`
+  margin: 150px 0;
+`;
 export const HomeDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: -25px;
+  //margin-top: 150px;
 `;
 
 export const HomeImage = styled.img`
@@ -46,4 +48,27 @@ export const SearchButton = styled.button`
   background-color: var(--main-color);
   border-radius: 44px;
   color: white;
+`;
+// tagList
+export const TagWrapper = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  padding: 20px 16px 0px 16px;
+  width: 400px;
+  margin-top: 50px;
+  margin: 50px auto 0 auto;
+  overflow-x: auto;
+  white-space: nowrap;
+`;
+
+export const TagList = styled.button`
+  display: inline-block; // 추가됨
+  padding: 10px 15px;
+  border: 1px solid #d9d9d9;
+  border-radius: 20px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+
+  ${({ clicked }) =>
+    clicked && 'background-color: var(--main-color); color:white;'}
 `;
